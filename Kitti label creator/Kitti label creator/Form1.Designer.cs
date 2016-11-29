4536de47f334e7678d41428179875c0ec3685e14
David Astorga <d21430@users.noreply.github.com>
2016-11-28 22:29:43 -0800

    First upload
    First working version. Bugs may be spectated and future major changes on program structure.

---
+++ Kitti label creator/Kitti label creator/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Kitti_label_creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CtrlPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rstBtn = new System.Windows.Forms.Button();
+            this.discBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.dirBtn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.startLbl = new System.Windows.Forms.Label();
+            this.startTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.yLbl = new System.Windows.Forms.Label();
+            this.xLbl = new System.Windows.Forms.Label();
+            this.yTxt = new System.Windows.Forms.TextBox();
+            this.xTxt = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picPnl = new System.Windows.Forms.Panel();
+            this.CtrlPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.picPnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CtrlPanel
+            // 
+            this.CtrlPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.CtrlPanel.Controls.Add(this.label1);
+            this.CtrlPanel.Controls.Add(this.progressBar);
+            this.CtrlPanel.Controls.Add(this.panel1);
+            this.CtrlPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.CtrlPanel.Location = new System.Drawing.Point(0, 371);
+            this.CtrlPanel.Name = "CtrlPanel";
+            this.CtrlPanel.Size = new System.Drawing.Size(848, 57);
+            this.CtrlPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(6, 20);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(439, 23);
+            this.progressBar.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.rstBtn);
+            this.panel1.Controls.Add(this.discBtn);
+            this.panel1.Controls.Add(this.saveBtn);
+            this.panel1.Controls.Add(this.dirBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(448, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 57);
+            this.panel1.TabIndex = 0;
+            // 
+            // rstBtn
+            // 
+            this.rstBtn.Location = new System.Drawing.Point(26, 22);
+            this.rstBtn.Name = "rstBtn";
+            this.rstBtn.Size = new System.Drawing.Size(75, 23);
+            this.rstBtn.TabIndex = 3;
+            this.rstBtn.Text = "&Reset";
+            this.rstBtn.UseVisualStyleBackColor = true;
+            this.rstBtn.Click += new System.EventHandler(this.rstBtn_Click);
+            // 
+            // discBtn
+            // 
+            this.discBtn.Location = new System.Drawing.Point(107, 22);
+            this.discBtn.Name = "discBtn";
+            this.discBtn.Size = new System.Drawing.Size(75, 23);
+            this.discBtn.TabIndex = 2;
+            this.discBtn.Text = "&Discart";
+            this.discBtn.UseVisualStyleBackColor = true;
+            this.discBtn.Click += new System.EventHandler(this.discBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(188, 22);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 1;
+            this.saveBtn.Text = "&Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // dirBtn
+            // 
+            this.dirBtn.Location = new System.Drawing.Point(269, 22);
+            this.dirBtn.Name = "dirBtn";
+            this.dirBtn.Size = new System.Drawing.Size(119, 23);
+            this.dirBtn.TabIndex = 0;
+            this.dirBtn.Text = "&Open Directory";
+            this.dirBtn.UseVisualStyleBackColor = true;
+            this.dirBtn.Click += new System.EventHandler(this.dirBtn_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel2.Controls.Add(this.startLbl);
+            this.panel2.Controls.Add(this.startTxt);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.yLbl);
+            this.panel2.Controls.Add(this.xLbl);
+            this.panel2.Controls.Add(this.yTxt);
+            this.panel2.Controls.Add(this.xTxt);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(669, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(179, 371);
+            this.panel2.TabIndex = 1;
+            // 
+            // startLbl
+            // 
+            this.startLbl.AutoSize = true;
+            this.startLbl.Location = new System.Drawing.Point(20, 149);
+            this.startLbl.Name = "startLbl";
+            this.startLbl.Size = new System.Drawing.Size(44, 13);
+            this.startLbl.TabIndex = 6;
+            this.startLbl.Text = "Start at:";
+            // 
+            // startTxt
+            // 
+            this.startTxt.Location = new System.Drawing.Point(67, 146);
+            this.startTxt.Name = "startTxt";
+            this.startTxt.Size = new System.Drawing.Size(100, 20);
+            this.startTxt.TabIndex = 5;
+            this.startTxt.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Target resolution";
+            // 
+            // yLbl
+            // 
+            this.yLbl.AutoSize = true;
+            this.yLbl.Location = new System.Drawing.Point(26, 67);
+            this.yLbl.Name = "yLbl";
+            this.yLbl.Size = new System.Drawing.Size(38, 13);
+            this.yLbl.TabIndex = 3;
+            this.yLbl.Text = "Height";
+            // 
+            // xLbl
+            // 
+            this.xLbl.AutoSize = true;
+            this.xLbl.Location = new System.Drawing.Point(29, 40);
+            this.xLbl.Name = "xLbl";
+            this.xLbl.Size = new System.Drawing.Size(35, 13);
+            this.xLbl.TabIndex = 2;
+            this.xLbl.Text = "Width";
+            // 
+            // yTxt
+            // 
+            this.yTxt.Location = new System.Drawing.Point(67, 64);
+            this.yTxt.Name = "yTxt";
+            this.yTxt.Size = new System.Drawing.Size(100, 20);
+            this.yTxt.TabIndex = 1;
+            this.yTxt.Text = "370";
+            // 
+            // xTxt
+            // 
+            this.xTxt.Location = new System.Drawing.Point(67, 37);
+            this.xTxt.Name = "xTxt";
+            this.xTxt.Size = new System.Drawing.Size(100, 20);
+            this.xTxt.TabIndex = 0;
+            this.xTxt.Text = "1224";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(657, 324);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // picPnl
+            // 
+            this.picPnl.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.picPnl.Controls.Add(this.pictureBox1);
+            this.picPnl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picPnl.Location = new System.Drawing.Point(0, 0);
+            this.picPnl.Name = "picPnl";
+            this.picPnl.Size = new System.Drawing.Size(669, 371);
+            this.picPnl.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 428);
+            this.Controls.Add(this.picPnl);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.CtrlPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_ResizeBegin);
+            this.CtrlPanel.ResumeLayout(false);
+            this.CtrlPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.picPnl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel CtrlPanel;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button rstBtn;
+        private System.Windows.Forms.Button discBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button dirBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel picPnl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label yLbl;
+        private System.Windows.Forms.Label xLbl;
+        private System.Windows.Forms.TextBox yTxt;
+        private System.Windows.Forms.TextBox xTxt;
+        private System.Windows.Forms.Label startLbl;
+        private System.Windows.Forms.TextBox startTxt;
+
+    }
+}
+
---
+++ Kitti label creator/Kitti label creator/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Text.RegularExpressions;
+
+namespace Kitti_label_creator
+{
+    public partial class Form1 : Form
+    {
+        Bitmap basePic, marks = new Bitmap(10, 10);
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+            createBase();
+            defPicSize();
+        }
+
+        List<string> files;
+        string dir;
+        int idx = 0;
+        double scale = 0;
+        string kitti = "";
+        List<double> points = new List<double>();
+        List<double> origin = new List<double>();
+        Regex exp = new Regex(@"//.+");
+        Pen blackPen = new Pen(Color.Red, 3);
+
+
+        private void dirBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            dialog.ShowDialog();
+            dir = dialog.SelectedPath;
+
+            IEnumerable<string> file = Directory.EnumerateFiles(dir + "//", "*.*").Where(s => s.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+                s.EndsWith(".png", StringComparison.OrdinalIgnoreCase) || s.EndsWith(".gif", StringComparison.OrdinalIgnoreCase) || s.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase) ||
+                s.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase));
+            files = file.ToList();
+            Directory.CreateDirectory(dir + "//images//");
+            Directory.CreateDirectory(dir + "//labels//");
+            Directory.CreateDirectory(dir + "//processed//");
+            Directory.CreateDirectory(dir + "//discarted//");
+            progressBar.Maximum = files.Count;
+            progressBar.Value = 0;
+            pictureBox1.BackgroundImage = nextPicture();
+            saveBtn.Focus();
+        }
+
+        Bitmap nextPicture()
+        {
+            Bitmap picture = newPic(files[idx]);
+            idx++;
+            label1.Text = "File " + idx.ToString() + " of " + files.Count.ToString();
+            progressBar.Value++;
+
+            return picture;
+        }
+
+        double[] relSize()
+        {
+            double relx = float.Parse(xTxt.Text) / picPnl.Width;
+            double rely = float.Parse(yTxt.Text) / picPnl.Height;
+
+            if (relx > rely)
+                return new double[] { relx, 0 };
+            return new double[] { rely, 1 };
+        }
+
+        void defPicSize()
+        {
+            double[] res = relSize();
+            scale = res[0];
+
+            if (res[1] == 0)
+            {
+                pictureBox1.Width = picPnl.Width;
+                pictureBox1.Height = (int)(int.Parse(yTxt.Text) / scale);
+            }
+            else
+            {
+                pictureBox1.Width = (int)(int.Parse(xTxt.Text) / scale);
+                pictureBox1.Height = picPnl.Height;
+            }
+            pictureBox1.Location = new Point((picPnl.Width - pictureBox1.Width) / 2, (picPnl.Height - pictureBox1.Height) / 2);
+        }
+
+        Bitmap newPic(string path)
+        {
+            again:
+            try
+            {
+                using (var picture = (Bitmap)Image.FromFile(files[idx]))
+                {
+                    double scale = 0, temp = 0;
+                    int[] offset = new int[2];
+
+                    if (picture.Height <= basePic.Height && picture.Width <= basePic.Width)
+                    {
+                        offset[0] = (basePic.Width - picture.Width) / 2;
+                        offset[1] = (basePic.Height - picture.Height) / 2;
+                        return place(picture, offset);
+
+                    }
+                    else
+                    {
+                        scale = (double)basePic.Width / picture.Width;
+                        temp = (double)basePic.Height / picture.Height;
+                        Size size;
+
+                        if (scale < temp)
+                        {
+                            size = new Size(basePic.Width, (int)(picture.Height * scale));
+
+                        }
+                        else
+                        {
+                            size = new Size((int)(picture.Width * temp), basePic.Height);
+                        }
+                        offset[0] = (basePic.Width - size.Width) / 2;
+                        offset[1] = (basePic.Height - size.Height) / 2;
+                        return place(new Bitmap(picture, size), offset);
+                    }
+                }
+            }
+            catch (SystemException)
+            {
+                string name = exp.Match(files[idx]).Value;
+                File.Move(files[idx], dir + "\\discarted" + name);
+                if (idx == files.Count)
+                {
+                    saveBtn.Enabled = false;
+                }
+                idx++;
+                goto again;
+            }
+        }
+
+        Bitmap place(Bitmap Picture, int[] offset)
+        {
+            for (int i = 0; i < Picture.Width; i++)
+            {
+                for (int a = 0; a < Picture.Height; a++)
+                {
+                    //Color test = Picture.GetPixel(i, a);
+                    basePic.SetPixel(i + offset[0], a + offset[1], Picture.GetPixel(i, a));
+                }
+            }
+            return basePic;
+        }
+
+        void createBase()
+        {
+            int x = int.Parse(xTxt.Text), y = int.Parse(yTxt.Text);
+            basePic = new Bitmap(x, y);
+            if (x != marks.Width || y != marks.Height)
+            {
+                marks.Dispose();
+                marks = new Bitmap(x, y);
+            }
+            pictureBox1.Image = marks;
+            for (int i = 0; i < x; i++)
+            {
+                for (int a = 0; a < y; a++)
+                {
+                    basePic.SetPixel(i, a, Color.Gray);
+                }
+            }
+            //pictureBox1.BackgroundImage = basePic;
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+            defPicSize();
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            for (int i = 4; i <= points.Count; i += 4)
+            {
+                kitti += "car 0.0 0 0.0 " + (points[i - 4] * scale).ToString() + " " +
+                    (points[i - 3] * scale).ToString() + " " + (points[i - 2] * scale).ToString() +
+                    " " + (points[i - 1] * scale).ToString() + " 0.0 0.0 0.0 0.0 0.0 0.0 0.0\r\n";
+            }
+
+            points.Clear();
+            origin.Clear();
+
+            string fileName = (idx + int.Parse(startTxt.Text) - 1).ToString();
+            while (fileName.Length < 5)
+                fileName = "0" + fileName;
+            pictureBox1.BackgroundImage.Save(dir + "\\images\\" + fileName + ".png", System.Drawing.Imaging.ImageFormat.Png);
+            File.WriteAllText(dir + "\\labels\\" + fileName + ".txt", kitti);
+            string name = exp.Match(files[idx - 1]).Value;
+            File.Move(files[idx - 1], dir + "\\processed" + name);
+            //nextPicture().Dispose();
+            basePic.Dispose();
+            createBase();
+            kitti = "";
+            if (idx == files.Count)
+            {
+                saveBtn.Enabled = false;
+                return;
+            }
+            pictureBox1.BackgroundImage = nextPicture();
+        }
+
+        bool marking = false;
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            marking = !marking;
+            if (marking)
+            {
+                points.Add(e.X);
+                points.Add(e.Y);
+                origin.Add(e.X);
+                origin.Add(e.Y);
+                points.Add(e.X * scale);
+                points.Add(e.Y * scale);
+            }
+            else
+                pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!marking)
+                return;
+            if (e.X > origin[origin.Count - 2])
+            {
+                points[points.Count - 2] = e.X;
+                points[points.Count - 4] = origin[origin.Count - 2];
+            }
+            else
+            {
+                points[points.Count - 4] = e.X;
+                points[points.Count - 2] = origin[origin.Count - 2];
+            }
+            if (e.Y > origin[origin.Count - 1])
+            {
+                points[points.Count - 1] = e.Y;
+                points[points.Count - 3] = origin[origin.Count - 1];
+            }
+            else
+            {
+                points[points.Count - 3] = e.Y;
+                points[points.Count - 1] = origin[origin.Count - 1];
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void discBtn_Click(object sender, EventArgs e)
+        {
+            string name = exp.Match(files[idx - 1]).Value;
+            File.Move(files[idx - 1], dir + "\\discarted" + name);
+            //nextPicture().Dispose();
+            basePic.Dispose();
+            createBase();
+            if (idx == files.Count)
+            {
+                saveBtn.Enabled = false;
+                return;
+            }
+            pictureBox1.BackgroundImage = nextPicture();
+            saveBtn.Focus();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            int limit = points.Count;
+            for (int i = 4; i <= limit; i += 4)
+            {
+                Rectangle rect = new Rectangle((int)points[i - 4], (int)points[i - 3],
+                    (int)(points[i - 2] - points[i - 4]), (int)(points[i - 1] - points[i - 3]));
+                e.Graphics.DrawRectangle(blackPen, rect);
+            }
+
+        }
+
+        private void rstBtn_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            origin.Clear();
+            pictureBox1.Refresh();
+            saveBtn.Focus();
+        }
+    }
+}
